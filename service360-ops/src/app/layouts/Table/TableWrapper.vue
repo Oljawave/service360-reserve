405d688dffa91782eb980abd78841e3d9bbf1277
Oljawave <misterolzhas04@gmail.com>
2025-12-25 16:29:37 +0500

    25.12T16:29_CopyWorkPlan

--- service360-ops/src/app/layouts/Table/TableWrapper.vue
+++ service360-ops/src/app/layouts/Table/TableWrapper.vue
@@ -62,8 +62,11 @@
       :sortKey="sortKey"
       :sortDirection="sortDirection"
       :getRowClassFn="getRowClassFn"
+      :showCheckbox="showCheckbox"
+      :selectedRows="selectedRows"
       @row-dblclick="handleRowDoubleClick" @toggle-filter="toggleFilter"
       @sort="handleSort"
+      @update:selectedRows="handleUpdateSelectedRows"
     >
       <template #filter="{ column }">
         <ColumnFilter
@@ -146,10 +149,18 @@ const props = defineProps({
   getRowClassFn: {
     type: Function,
     default: () => ({}),
+  },
+  showCheckbox: {
+    type: Boolean,
+    default: false
+  },
+  selectedRows: {
+    type: Array,
+    default: () => []
   }
 });
 
-const emit = defineEmits(['update:filters', 'row-dblclick']);
+const emit = defineEmits(['update:filters', 'row-dblclick', 'update:selectedRows']);
 
 const rows = ref([]);
 const childrenMap = ref({});
@@ -295,6 +306,10 @@ const handleSave = async () => {
 
 const refreshTable = () => {
   loadData();
+};
+
+const handleUpdateSelectedRows = (newSelectedRows) => {
+  emit('update:selectedRows', newSelectedRows);
 };
 
 const handleSort = (key) => {
